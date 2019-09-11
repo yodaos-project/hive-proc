87e2988cbad182662df1555e08ecf327c94f3ea7
legendecas <legendecas@gmail.com>
2019-09-11 15:49:31 +0800

    src: logger printf time

--- src/logger.h
+++ src/logger.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <time.h>
 
-#define YDLog_VA(out, msg, ...) \
-  fprintf(out, "%s(%s:%d): " msg "\n", LOG_TAG, __FILE__, __LINE__, ##__VA_ARGS__)
+#define YDLog_VA(out, msg, ...)                                                                  \
+  {                                                                                              \
+    time_t timer;                                                                                \
+    char buffer[26];                                                                             \
+    struct tm *tm_info;                                                                          \
+    time(&timer);                                                                                \
+    tm_info = localtime(&timer);                                                                 \
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);                                          \
+    fprintf(out, "%s %s(%s:%d): " msg "\n", buffer, LOG_TAG, __FILE__, __LINE__, ##__VA_ARGS__); \
+  }
 
 // YDLogve(message)
 #define YDLogv(...) YDLog_VA(stdout, __VA_ARGS__)
