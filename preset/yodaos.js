3d0cd6ffe881379dd9cb4d3c4f15d520c1fdd8bc
legendecas <legendecas@gmail.com>
2019-09-24 13:55:47 +0800

    preset: remove flora and direct dependencies on flora

--- preset/yodaos.js
+++ preset/yodaos.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-unused-expressions,import/no-absolute-path */
-require('/usr/yoda/client/ext-helper.js')
 require('@yodaos/application').Application
 require('@yodaos/application').AudioFocus
 require('@yodaos/application').Service
-require('@yoda/oh-my-little-pony')
 require('@yoda/util')
 require('/usr/yoda/client/api/default.json')
